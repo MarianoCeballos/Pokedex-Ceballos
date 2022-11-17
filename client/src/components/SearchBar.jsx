14491698628f3c75c25bb1c8a3b13c73e7cfb41b
Mariano Ceballos Torres <98489048+MarianoCeballos@users.noreply.github.com>
2022-11-17 09:59:38 -0300

    changes in search

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,42 +1,37 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { getPokemonBySearch, setPage } from '../redux/actions/actions';
+import { getPokemonBySearch } from '../redux/actions/actions';
 import s from '../Styles/SearchBar.module.css';
-import { useHistory } from 'react-router-dom';
 
-function SearchBar() {
-  const history = useHistory();
-  const dispatch = useDispatch();
-  const [search, setSearch] = useState('');
+const SearchBar = () => {
+    const dispatch = useDispatch();
+    const [search, setSearch] = useState('');
 
-  function handleClick(e) {
-    e.preventDefault();
-    if (search) {
-      dispatch(getPokemonBySearch(search.toLowerCase())).then((r) => {
-        if (r) {
-          dispatch(setPage(1));
-        }
-        history.push('/home');
-        setSearch('');
-      });
-    }
-  }
+    const handleChange = (e) => {
+        e.preventDefault();
+        setSearch(e.target.value);
+        dispatch(getPokemonBySearch(e.target.value));
+    };
 
-  return (
-    <form onSubmit={(e) => handleClick(e)} className={s.search}>
-      <div className={s.input}>
-        <input
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          type='text'
-          placeholder='Search pokémon'
-        />
-      </div>
-      <div>
-        <input className={s.searchButton} type='submit' value='Search' />
-      </div>
-    </form>
-  );
-}
+    return (
+        <form className={s.search}>
+            <div className={s.input}>
+                <input
+                    value={search}
+                    onChange={(e) => handleChange(e)}
+                    type='text'
+                    placeholder='Search pokémon'
+                />
+            </div>
+            <div>
+                <input
+                    className={s.searchButton}
+                    type='submit'
+                    value='Search'
+                />
+            </div>
+        </form>
+    );
+};
 
 export default SearchBar;
--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 import {
-  GET_POKEMONS,
-  FILTERED_POKEMONS,
-  CREATE_POKEMON,
-  GET_TYPES,
-  GET_POKEMON_BY_PARAM,
-  ORDERED_POKEMONS_ATTACK,
-  ORDERED_POKEMONS_NAME,
-  FILTER_BY_TYPE,
-  SET_PAGE,
-  LOADING,
-  DELETE_POKEMON,
-  GET_POKEMON_DETAIL,
+    GET_POKEMONS,
+    FILTERED_POKEMONS,
+    CREATE_POKEMON,
+    GET_TYPES,
+    GET_POKEMON_BY_PARAM,
+    ORDERED_POKEMONS_ATTACK,
+    ORDERED_POKEMONS_NAME,
+    FILTER_BY_TYPE,
+    SET_PAGE,
+    LOADING,
+    DELETE_POKEMON,
+    GET_POKEMON_DETAIL,
 } from './actionTypes';
 import Swal from 'sweetalert2';
 //
@@ -22,182 +22,171 @@ axios.defaults.baseURL = render;
 //GETS----------------------------------------------------------
 
 export function getPokemons() {
-  return async function(dispatch) {
-    try {
-      const response = await axios.get(`/pokemons`);
-      const pokemons = await response.data;
-      dispatch({ type: GET_POKEMONS, payload: pokemons });
-    } catch (error) {
-      return error;
-    }
-  };
+    return async function(dispatch) {
+        try {
+            const response = await axios.get(`/pokemons`);
+            const pokemons = await response.data;
+            dispatch({ type: GET_POKEMONS, payload: pokemons });
+        } catch (error) {
+            return error;
+        }
+    };
 }
 export function clearPokemons() {
-  return async function(dispatch) {
-    try {
-      dispatch({ type: GET_POKEMON_DETAIL, payload: {} });
-    } catch (error) {
-      return error;
-    }
-  };
+    return async function(dispatch) {
+        try {
+            dispatch({ type: GET_POKEMON_DETAIL, payload: {} });
+        } catch (error) {
+            return error;
+        }
+    };
 }
 
 export function getPokemonDetail(param) {
-  return async function(dispatch) {
-    try {
-      const pokemon = (await axios.get(`/pokemons/${param}`)).data;
+    return async function(dispatch) {
+        try {
+            const pokemon = (await axios.get(`/pokemons/${param}`)).data;
 
-      if (!pokemon || !pokemon.data) {
-        return Swal.fire({
-          title: `No pokemon found with name ${param}`,
-          icon: 'warning',
-          dangerMode: true,
-        }).then(() => {
-          return false;
-        });
-      } else {
-        dispatch({ type: GET_POKEMON_DETAIL, payload: pokemon.data });
-        return true;
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+            if (!pokemon || !pokemon.data) {
+                return Swal.fire({
+                    title: `No pokemon found with name ${param}`,
+                    icon: 'warning',
+                    dangerMode: true,
+                }).then(() => {
+                    return false;
+                });
+            } else {
+                dispatch({ type: GET_POKEMON_DETAIL, payload: pokemon.data });
+                return true;
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
 }
 
 export function getPokemonBySearch(param) {
-  return async function(dispatch) {
-    const pokemon = (await axios.get(`/pokemons/${param}?search=true`)).data;
-    try {
-      if (!pokemon.data.length) {
-        return Swal.fire({
-          title: `No pokemon found with name ${param}`,
-          icon: 'warning',
-          dangerMode: true,
-        }).then(() => {
-          return false;
-        });
-      } else {
-        dispatch({ type: GET_POKEMON_BY_PARAM, payload: pokemon.data });
-      }
-      return pokemon;
-    } catch (err) {
-      console.log(err);
-      return;
-    }
-  };
+    return async function(dispatch) {
+        //const pokemon = (await axios.get(`/pokemons/${param}?search=true`)).data;
+        try {
+            dispatch({ type: GET_POKEMON_BY_PARAM, payload: param });
+        } catch (err) {
+            console.log(err);
+            return;
+        }
+    };
 }
 
 export function getTypes() {
-  return async function(dispatch) {
-    try {
-      const response = await axios.get(`/types`);
-      const types = await response.data;
-      return dispatch({ type: GET_TYPES, payload: types });
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    return async function(dispatch) {
+        try {
+            const response = await axios.get(`/types`);
+            const types = await response.data;
+            return dispatch({ type: GET_TYPES, payload: types });
+        } catch (error) {
+            console.error(error);
+        }
+    };
 }
 
 //FILTERS--------------------------------------------------------
 
 export function filterPokemons(filter) {
-  return async function(dispatch) {
-    if (filter === 'Created') {
-      dispatch({ type: FILTERED_POKEMONS, payload: filter });
-    } else if (filter === 'Api') {
-      dispatch({ type: FILTERED_POKEMONS, payload: filter });
-    } else if (filter === 'All') {
-      dispatch({ type: FILTERED_POKEMONS, payload: filter });
-    } else {
-      dispatch({ type: FILTER_BY_TYPE, payload: filter });
-    }
-  };
+    return async function(dispatch) {
+        if (filter === 'Created') {
+            dispatch({ type: FILTERED_POKEMONS, payload: filter });
+        } else if (filter === 'Api') {
+            dispatch({ type: FILTERED_POKEMONS, payload: filter });
+        } else if (filter === 'All') {
+            dispatch({ type: FILTERED_POKEMONS, payload: filter });
+        } else {
+            dispatch({ type: FILTER_BY_TYPE, payload: filter });
+        }
+    };
 }
 //ORDER-------------------------------------------------------------------------
 
 export function orderPokemons(order) {
-  return async function(dispatch) {
-    if (order === 'max' || order === 'min') {
-      dispatch({ type: ORDERED_POKEMONS_ATTACK, payload: order });
-    } else {
-      dispatch({ type: ORDERED_POKEMONS_NAME, payload: order });
-    }
-  };
+    return async function(dispatch) {
+        if (order === 'max' || order === 'min') {
+            dispatch({ type: ORDERED_POKEMONS_ATTACK, payload: order });
+        } else {
+            dispatch({ type: ORDERED_POKEMONS_NAME, payload: order });
+        }
+    };
 }
 
 //PAGINATION----------------------------------------------------------------------
 
 export function setPage(page) {
-  return function(dispatch) {
-    dispatch({ type: SET_PAGE, payload: page });
-  };
+    return function(dispatch) {
+        dispatch({ type: SET_PAGE, payload: page });
+    };
 }
 
 //POST----------------------------------------------------------------------------
 
 export function createPokemon(pokemon) {
-  return async function(dispatch) {
-    try {
-      const response = (await axios.post(`/pokemons`, pokemon)).data;
-      dispatch({
-        type: CREATE_POKEMON,
-        payload: pokemon,
-      });
-      return Swal.fire({
-        title: 'Pokemon created!',
-        text: response.data.name,
-        icon: 'success',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        cancelButtonText: 'OK',
-        confirmButtonText: 'Show pokemon',
-      }).then((result) => {
-        if (result.isConfirmed) {
-          return response.data.name;
+    return async function(dispatch) {
+        try {
+            const response = (await axios.post(`/pokemons`, pokemon)).data;
+            dispatch({
+                type: CREATE_POKEMON,
+                payload: pokemon,
+            });
+            return Swal.fire({
+                title: 'Pokemon created!',
+                text: response.data.name,
+                icon: 'success',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                cancelButtonText: 'OK',
+                confirmButtonText: 'Show pokemon',
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    return response.data.name;
+                }
+            });
+        } catch (error) {
+            alert(error.response.data.message);
+            return error;
         }
-      });
-    } catch (error) {
-      alert(error.response.data.message);
-      return error;
-    }
-  };
+    };
 }
 
 //LOADING -------------------------------------------------------------------------
 
 export function setLoading(value) {
-  return function(dispatch) {
-    dispatch({ type: LOADING, payload: value });
-  };
+    return function(dispatch) {
+        dispatch({ type: LOADING, payload: value });
+    };
 }
 //DELETE -----------------------------------------------------------------------------
 export function deletePokemon(id) {
-  return async function(dispatch) {
-    try {
-      const response = (await axios.delete(`/pokemons/${id}`)).data;
-      dispatch({ type: DELETE_POKEMON, payload: id });
+    return async function(dispatch) {
+        try {
+            const response = (await axios.delete(`/pokemons/${id}`)).data;
+            dispatch({ type: DELETE_POKEMON, payload: id });
 
-      Swal.fire({
-        title: 'Deleted',
-        text: `Pokemon ${response.data.name} deleted!`,
-        icon: 'success',
-        button: 'OK',
-      });
+            Swal.fire({
+                title: 'Deleted',
+                text: `Pokemon ${response.data.name} deleted!`,
+                icon: 'success',
+                button: 'OK',
+            });
 
-      return response.data.message;
-    } catch (error) {
-      throw Error(error);
-    }
-  };
+            return response.data.message;
+        } catch (error) {
+            throw Error(error);
+        }
+    };
 }
 
 export function updatePokemon(pokemon, id) {
-  return async function() {
-    try {
-      await axios.put(`/pokemons/${id}`, pokemon);
-    } catch (error) {}
-  };
+    return async function() {
+        try {
+            await axios.put(`/pokemons/${id}`, pokemon);
+        } catch (error) {}
+    };
 }
